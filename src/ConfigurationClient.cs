a1a21c0c907b480758a47b514a6a870172a510bf
Mark A. Nicolosi <mark.a.nicolosi@gmail.com>
2007-06-01 06:29:08 -0500

    Cleaned up ConfigurationClient.cs
    * Moved static method SyncToggleActionHandler() towards the top of the
      class.
    * Added helper private method GetFullKeyName to create a string of the
      full path to a key.

--- src/ConfigurationClient.cs
+++ src/ConfigurationClient.cs
@@ -27,51 +27,6 @@ namespace Questar.Configuration
         }
 
         private const string base_key = "/apps/questar/";
-        private Client client;
-
-        private ConfigurationClient ()
-        {
-            client = new Client ();
-        }
-
-        public void AddNotify<T> (SchemaEntry<T> entry, NotifyEventHandler handler)
-        {
-            client.AddNotify (base_key + entry.Namespace + "/" + entry.Key,
-                handler);
-        }
-
-        public T Get<T> (SchemaEntry<T> entry)
-        {
-            string key = base_key + entry.Namespace + "/" + entry.Key;
-
-            try {
-                object value = client.Get (key);
-
-                if (typeof (T).IsEnum)
-                    value = Enum.Parse (typeof (T), value as string, true);                    
-
-                return (T) value;
-            }
-            catch (NoSuchKeyException) {
-                Set<T> (entry, entry.DefaultValue);
-                return entry.DefaultValue;
-            }
-            catch (ArgumentException) {
-                Set<T> (entry, entry.DefaultValue);
-                return entry.DefaultValue;
-            }
-        }
-
-        public void Set<T> (SchemaEntry<T> entry, T value)
-        {
-            object real_value = value;
-            string key = base_key + entry.Namespace + "/" + entry.Key;
-
-            if (typeof (T).IsEnum)
-                real_value = value.ToString ();
-
-            client.Set (key, real_value);
-        }
 
         public delegate void SyncToggleActionHandler (ToggleAction action,
             SchemaEntry<bool> entry);
@@ -89,6 +44,57 @@ namespace Questar.Configuration
                 entry.Set (action.Active);
                 handler (action, entry);
             };
+        }
+
+        private Client client;
+
+        private ConfigurationClient ()
+        {
+            client = new Client ();
+        }
+
+        public void AddNotify<T> (SchemaEntry<T> entry,
+            NotifyEventHandler handler)
+        {
+            client.AddNotify (GetFullKeyName<T> (entry), handler);
+        }
+
+        public T Get<T> (SchemaEntry<T> entry)
+        {
+            string key = GetFullKeyName<T> (entry);
+
+            try {
+                object value = client.Get (key);
+
+                if (typeof (T).IsEnum)
+                    value = Enum.Parse (typeof (T), value as string, true);
+
+                return (T) value;
+            }
+            catch (NoSuchKeyException) {
+                Set<T> (entry, entry.DefaultValue);
+                return entry.DefaultValue;
+            }
+            catch (ArgumentException) {
+                Set<T> (entry, entry.DefaultValue);
+                return entry.DefaultValue;
+            }
+        }
+
+        public void Set<T> (SchemaEntry<T> entry, T value)
+        {
+            object real_value = value;
+            string key = GetFullKeyName<T> (entry);
+
+            if (typeof (T).IsEnum)
+                real_value = value.ToString ();
+
+            client.Set (key, real_value);
+        }
+
+        private string GetFullKeyName<T> (SchemaEntry<T> entry)
+        {
+            return base_key + entry.Namespace + "/" + entry.Key;
         }
     }
 }
