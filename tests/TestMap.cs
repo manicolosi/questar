af72af28d40ce537e6bb73c446f193bfbefeafcc
Mark A. Nicolosi <mark.a.nicolosi@gmail.com>
2007-06-23 01:32:34 -0500

    Unit Tests for Map.FillArea()

---
+++ tests/TestMap.cs
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ *  TestActor.cs: Unit Tests for Actor class.
+ *
+ *  Copyright (C) 2007
+ *  Written by Mark A. Nicolosi <mark.a.nicolosi@gmail.com>
+ ******************************************************************************/
+
+using NUnit.Framework;
+using System;
+
+using Questar.Base;
+using Questar.Maps;
+
+namespace Questar.UnitTests
+{
+    [TestFixture]
+    public class TestMap
+    {
+        Map map;
+
+        [SetUp]
+        public void Setup ()
+        {
+            World world = new World ();
+            Game.Instance.World = world;
+
+            world.Map = new Map ();
+            map = world.Map;
+        }
+
+        [Test]
+        public void FillArea ()
+        {
+            int w = map.Width;
+            int h = map.Height;
+            map.FillArea ("flower", new Rectangle (0, 0, w, h));
+
+            for (int i = 0; i < w; i++) {
+                for (int j = 0; j < h; j++) {
+                    Assert.AreEqual ("flower", map[i, j].Terrain.Id);
+                }
+            }
+        }
+    }
+}
+
