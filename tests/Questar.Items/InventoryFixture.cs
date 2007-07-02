38f8b77cd5c365330769347a38dab4dd91a80ee3
Mark A. Nicolosi <mark.a.nicolosi@gmail.com>
2007-07-02 05:30:19 -0500

    New test fixture InventoryFixture

---
+++ tests/Questar.Items/InventoryFixture.cs
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ *  InventoryFixture.cs: Unit Tests for Inventory objects.
+ *
+ *  Copyright (C) 2007
+ *  Written by Mark A. Nicolosi <mark.a.nicolosi@gmail.com>
+ ******************************************************************************/
+
+using NUnit.Framework;
+
+using Questar.Items;
+
+namespace Questar.UnitTests.Items
+{
+    [TestFixture]
+    public class InventoryFixture
+    {
+    }
+}
+
