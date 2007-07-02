ab65c9afca0cab674d916f414ad5f416105d9e5d
Mark A. Nicolosi <mark.a.nicolosi@gmail.com>
2007-07-02 01:27:35 -0500

    New Item class HealthPotion

---
+++ src/Questar.Items/HealthPotion.cs
@@ -0,0 +1,17 @@
+/*******************************************************************************
+ *  HealthPotion.cs: Implements a health potion Item.
+ *
+ *  Copyright (C) 2007
+ *  Written by Mark A. Nicolosi <mark.a.nicolosi@gmail.com>
+ ******************************************************************************/
+
+namespace Questar.Items
+{
+    public class HealthPotion : Item
+    {
+        public HealthPotion ()
+        {
+        }
+    }
+}
+
