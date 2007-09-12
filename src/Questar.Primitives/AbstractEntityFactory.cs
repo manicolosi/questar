39db70d7eee1f09906aab05ac364f8a7086aaa39
Mark A. Nicolosi <mark.a.nicolosi@gmail.com>
2007-09-12 12:50:05 -0500

    Created abstract implementation of IEntityFactory<T>
        Created EntityCreatedEventArgs and changed event Created in
        IEntityFactory to use it.

---
+++ src/Questar.Primitives/AbstractEntityFactory.cs
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ *  AbstractEntityFactory.cs: An abstract implementation of
+ *  IEntityFactory.
+ *
+ *  Copyright (C) 2007
+ *  Written by Mark A. Nicolosi <mark.a.nicolosi@gmail.com>
+ ******************************************************************************/
+
+using System;
+
+using Questar.Helpers;
+
+namespace Questar.Primitives
+{
+    public abstract class AbstractEntityFactory<T> where T: Entity,
+        IEntityFactory<T>
+    {
+        event EventHandler<EntityCreatedEventArgs> Created;
+
+        public abstract T Create (string id);
+
+        protected void OnCreation (Entity entity)
+        {
+            EventHelper.Raise<EntityCreatedEventArgs> (this, Created,
+                delegate (EntityCreatedEventArgs args) {
+                    args.Entity = entity;
+                });
+        }
+    }
+}
+
--- src/Questar.Primitives/IEntityFactory.cs
+++ src/Questar.Primitives/IEntityFactory.cs
@@ -10,10 +10,15 @@ using System;
 
 namespace Questar.Primitives
 {
+    public class EntityCreatedEventArgs : EventArgs
+    {
+        public Entity Entity;
+    }
+
     public interface IEntityFactory<T> where T: Entity
     {
+        event EventHandler<EntityCreatedEventArgs> Created;
         T Create (string id);
-        event EventHandler<EventArgs> Created;
     }
 }
 
