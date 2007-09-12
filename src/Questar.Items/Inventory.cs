b8bb4004d0109d2526f38987a91adef055e7343a
Mark A. Nicolosi <mark.a.nicolosi@gmail.com>
2007-09-12 16:11:49 -0500

    New events for Inventory: Added and Removed
        Created a private helper method: FireEvent() that can fire either
        the Added or the Removed events.

--- src/Questar.Items/Inventory.cs
+++ src/Questar.Items/Inventory.cs
@@ -10,12 +10,21 @@ using System.Collections;
 using System.Collections.Generic;
 
 using Questar.Actors;
+using Questar.Helpers;
 using Questar.Primitives;
 
 namespace Questar.Items
 {
+    public class ItemEventArgs : EventArgs
+    {
+        public Item Item;
+    }
+
     public class Inventory : IEnumerable<Item>
     {
+        public event EventHandler<ItemEventArgs> Added;
+        public event EventHandler<ItemEventArgs> Removed;
+
         private Actor owner;
         private List<Item> items = new List<Item> ();
 
@@ -48,6 +57,8 @@ namespace Questar.Items
 
             item.Location = new ActorLocation (owner);
             items.Add (item);
+
+            FireEvent (Added, item);
         }
 
         public void Remove (Item item)
@@ -59,6 +70,8 @@ namespace Questar.Items
                 throw new ArgumentException ("Item is not in this Inventory.");
 
             items.Remove (item);
+
+            FireEvent (Removed, item);
         }
 
         public bool Contains (Item item)
@@ -74,6 +87,14 @@ namespace Questar.Items
         IEnumerator IEnumerable.GetEnumerator ()
         {
             return GetEnumerator ();
+        }
+
+        private void FireEvent (EventHandler<ItemEventArgs> event_handler, Item item)
+        {
+            EventHelper.Raise<ItemEventArgs> (this, event_handler,
+                delegate (ItemEventArgs args) {
+                    args.Item = item;
+                });
         }
     }
 }
